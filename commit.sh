9885290afe606530cc58bfe2bd0dbed673314345
Andrew Eastwood <ua.clients.support@gmail.com>
2014-03-24 10:22:33 +0200

    "cleanup"

--- commit.sh
+++ commit.sh
@@ -1,1 +1,12 @@
-git pull && git add . && git add . -u && git commit -m 'hotfix' && git push && git status
+#!/bin/sh
+
+echo "commiting changes"
+echo "========================"
+echo "git pull"
+echo "git add ."
+echo "git add . -u"
+echo "git commit -m \"$1\""
+echo "git push"
+echo "git status"
+
+git pull && git add . && git add . -u && git commit -m \"$1\" && git push && git status
