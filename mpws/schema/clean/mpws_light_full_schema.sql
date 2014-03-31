d5ba47c9995831617859888d2f05d96a1512fd2c
Andrew Eastwood <ua.clients.support@gmail.com>
2014-04-01 01:57:35 +0300

    updated commit script

--- mpws/schema/clean/mpws_light_full_schema.sql
+++ mpws/schema/clean/mpws_light_full_schema.sql
@@ -509,4 +509,4 @@ CREATE TABLE `toolbox_admins` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-04-01  1:56:34
+-- Dump completed on 2014-04-01  1:57:35
