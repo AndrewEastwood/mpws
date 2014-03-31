079701bfbbdbdb47ee1ee108b93f4b6ba446e359
Andrew Ivaskevych <AndrewEastwood@users.noreply.github.com>
2014-03-31 18:17:50 +0300

    Create sublime-user-settings.js

---
+++ mpws/schema/docs/sublime-user-settings.js
@@ -0,0 +1,20 @@
+{
+    "draw_white_space": "all",
+    "font_size": 10,
+    "bold_folder_labels": true,
+    "fade_fold_buttons": false,
+    "highlight_modified_tabs": true,
+    "highlight_line": true,
+    "font_face": "Inconsolata",
+    "font_size": 11,
+    "line_padding_bottom": 1,
+    "line_padding_top": 1,
+    "ignored_packages":
+    [
+        "Vintage"
+    ],
+    "translate_tabs_to_spaces": true,
+    "theme": "Spacegray.sublime-theme",
+    "color_scheme": "Packages/Theme - Spacegray/base16-ocean.dark.tmTheme"
+
+}
