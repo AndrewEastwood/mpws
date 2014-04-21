d2544428e55472a4fe89eedbdfb9ff2880ca2986
Andrew Eastwood <ua.clients.support@gmail.com>
2014-04-22 01:21:32 +0300

    adding shop dashboard

--- mpws/schema/clean/mpws_light_full_schema.sql
+++ mpws/schema/clean/mpws_light_full_schema.sql
@@ -516,4 +516,4 @@ CREATE TABLE `toolbox_admins` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-04-22  1:18:32
+-- Dump completed on 2014-04-22  1:21:32
